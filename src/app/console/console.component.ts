03bd10c0dcaa93eb42ac684fa2bbac1eeb5b89e8
BlackAkr <BlackAkr@gmail.com>
2021-06-21 15:55:56 +0300

    first commit

---
+++ src/app/console/console.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-console',
+  templateUrl: './console.component.html',
+  styleUrls: ['./console.component.scss']
+})
+export class ConsoleComponent implements OnInit {
+  @Input() value:String
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
